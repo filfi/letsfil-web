00a36e9461a31c25cbdb0f0d3ace3e3bc2e840e5
Steven <986535362@qq.com>
2023-07-11 19:05:19 +0800

    fix: fixed reward rate

--- src/hooks/useRaiseRate.ts
+++ src/hooks/useRaiseRate.ts
@@ -1,6 +1,7 @@
 import { useMemo } from 'react';
 
 import * as U from '@/utils/utils';
+import { toFixed } from '@/utils/format';
 
 /**
  * 节点计划的各方权益
@@ -15,13 +16,13 @@ export default function useRaiseRate(data?: API.Plan | null) {
   // 保证金占比
   const opsRatio = useMemo(() => U.accSub(data?.ops_security_fund_rate ?? 5, 0), [data?.ops_security_fund_rate]);
   // 建设者权益
-  const investRate = useMemo(() => U.accMul(priorityRate, U.accDiv(U.accSub(100, opsRatio), 100)), [priorityRate, opsRatio]);
+  const investRate = useMemo(() => toFixed(U.accMul(priorityRate, U.accDiv(U.accSub(100, opsRatio), 100)), 2), [priorityRate, opsRatio]);
   // 保证金权益
-  const opsRate = useMemo(() => U.accMul(priorityRate, U.accDiv(opsRatio, 100)), [priorityRate, opsRatio]);
+  const opsRate = useMemo(() => toFixed(U.accMul(priorityRate, U.accDiv(opsRatio, 100)), 2), [priorityRate, opsRatio]);
   // 服务商权益
   const servicerRate = useMemo(() => U.accSub(data?.op_server_share ?? 5, 0), [data?.op_server_share]);
   // filfi 协议权益
-  const ffiRate = useMemo(() => U.accMul(inferiorityRate, 0.08), [inferiorityRate]);
+  const ffiRate = useMemo(() => toFixed(U.accMul(inferiorityRate, 0.08), 2, 2), [inferiorityRate]);
   // 主办人权益
   const raiserRate = useMemo(() => U.accSub(inferiorityRate, ffiRate, servicerRate), [inferiorityRate, ffiRate, servicerRate]);
 
