c3fd4b326be673cf45d452e82b9670438c38457a
Steven <986535362@qq.com>
2023-07-20 10:29:24 +0800

    fix: switch to testnet

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -10,7 +10,7 @@ console.log('[RUN_ENV]: ', RUN_ENV);
 
 export const defaultLocale = 'zh-CN';
 
-export const isMainnet = ['main', 'staging'].includes(RUN_ENV);
+export const isMainnet = RUN_ENV === 'main';
 
 export const SUPPORTED_CHAINS = isMainnet
   ? [
@@ -28,7 +28,7 @@ const RAISE_ADDR = '0x390CB629D5057AB6F990471a725179FdfB64dEFD'; // 5000
 /**
  * Raise Facory Contract Address
  */
-export const RAISE_ADDRESS = (isMainnet ? ADDRESS : RAISE_ADDR) as API.Address;
+export const RAISE_ADDRESS = (ADDRESS || RAISE_ADDR) as API.Address;
 
 const SCAN_URL_MAIN = 'https://filfox.info/en'; // mainnet
 const SCAN_URL_CB = 'https://calibration.filscan.io'; // calibration testnet
