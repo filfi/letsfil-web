7cccc81332f461373ff88457a918522835f544d2
Steven <986535362@qq.com>
2023-08-21 17:21:10 +0800

    fix: fixed issues

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -27,13 +27,6 @@ export const config = createConfig({
 
 export const defaultWallet = 'MetaMask';
 
-export const blocklist = ['f02220887'];
+export const blocklist = [];
 
-export const whitelist = [
-  { address: '0x20688564aD8e1204A9Aaf895359eD57a0274185f', limit: 0 },
-  { address: '0xEE2BD6C12C5b1AC3b7B40b461EA92de70a926D8b', limit: 0 },
-  { address: '0x82BDEbDA0F6EBcaF25157Fdf8208c45b1881f028', limit: 0 },
-  { address: '0xC959e19E18093A5D2f3e4a007f32dE863ccfE48A', limit: 0 },
-  { address: '0x6c7434feb871d105b1ddf613d941c499ee3b36b0', limit: 0 },
-  { address: '0x82BDEbDA0F6EBcaF25157Fdf8208c45b1881f028', limit: 0 },
-];
+export const whitelist = [];
