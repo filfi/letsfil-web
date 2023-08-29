f215a0417b903b1158e96a35a16a94e0be7b7746
Steven <986535362@qq.com>
2023-08-29 15:46:39 +0800

    fix: change config

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -27,6 +27,11 @@ export const config = createConfig({
 
 export const defaultWallet = 'MetaMask';
 
-export const blocklist: string[] = [];
+export const blocklist: string[] = ['f02200000'];
 
-export const whitelist: { address: string; limit?: number }[] = [];
+export const whitelist: { address: string; limit?: number }[] = [
+  { address: '0xa9411db7afb7b23A7dDaAB3aB8e92DEe9186488E', limit: 0 },
+  { address: '0x2EfF86B8B7df28B47fA061A0b4B6D2bBBbD8C38d', limit: 0 },
+  { address: '0x5c20Da1090E22B313cc660a904F82107f05b6a05', limit: 0 },
+  { address: '0x6c7434feb871d105b1ddf613d941c499ee3b36b0', limit: 0 },
+];
