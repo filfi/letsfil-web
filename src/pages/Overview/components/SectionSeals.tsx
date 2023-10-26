66d00e72f1718791808f225a59dac9cf91022f12
Steven <986535362@qq.com>
2023-10-26 20:36:31 +0800

    fix: fixed views

--- src/pages/Overview/components/SectionSeals.tsx
+++ src/pages/Overview/components/SectionSeals.tsx
@@ -9,13 +9,13 @@ import useRaiseState from '@/hooks/useRaiseState';
 import useDepositRaiser from '@/hooks/useDepositRaiser';
 
 function fmtWidth(val: number) {
-  if (val >= 0.9) return Math.ceil(val) * 100;
+  if (val >= 0.9 && val < 1) return Math.ceil(val) * 100;
 
   return val * 100;
 }
 
 function fmtProgress(progress: number) {
-  if (progress >= 0.9) {
+  if (progress >= 0.9 && progress < 1) {
     return F.formatRate(Math.ceil(progress));
   }
 
