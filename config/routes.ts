fd4d4d55e98ce4c6f0af382138c8b9e1cf1688f6
Steven <986535362@qq.com>
2024-04-12 22:07:55 +0800

    feat(notice): update views

--- config/routes.ts
+++ config/routes.ts
@@ -7,6 +7,7 @@ export type IBestAFSRoute = {
   title?: string;
   wrappers?: string[];
   routes?: IBestAFSRoute[];
+  layout?: boolean;
   // 更多功能查看
   // https://beta-pro.ant.design/docs/advanced-menu
   // ---
@@ -36,115 +37,121 @@ const routes: IBestAFSRoute[] = [
   {
     path: '/',
     name: 'home',
-    component: './Home',
+    component: './Notice',
+    layout: false,
   },
-  {
-    name: 'account',
-    path: '/account',
-    component: './account/layout',
-    routes: [
-      {
-        name: 'assets',
-        path: 'assets',
-        component: './account/Assets',
-      },
-      {
-        name: 'plans',
-        path: 'plans',
-        component: './account/Plans',
-      },
-      {
-        name: 'stats',
-        path: 'stats',
-        component: './account/Stats',
-      },
-      { path: '/account', redirect: '/account/plans' },
-    ],
-  },
-  {
-    name: 'raising',
-    path: 'raising',
-    component: './Raising',
-  },
-  {
-    name: 'create',
-    path: 'create',
-    component: './create/layout',
-    routes: [
-      {
-        name: 'storage',
-        path: 'storage',
-        component: './create/Storage',
-      },
-      {
-        name: 'program',
-        path: 'program',
-        component: './create/Program',
-      },
-      {
-        name: 'benefit',
-        path: 'benefit',
-        component: './create/Benefit',
-      },
-      {
-        name: 'result',
-        path: 'result/:id',
-        component: './create/Result',
-      },
-      { path: '/create', redirect: '/create/storage' },
-    ],
-  },
-  {
-    name: 'mount',
-    path: 'mount',
-    component: './mount/layout',
-    routes: [
-      {
-        name: 'storage',
-        path: 'storage',
-        component: './mount/Storage',
-      },
-      {
-        name: 'benefit',
-        path: 'benefit',
-        component: './mount/Benefit',
-      },
-      {
-        name: 'result',
-        path: 'result/:id',
-        component: './mount/Result',
-      },
-      { path: '/mount', redirect: '/mount/storage' },
-    ],
-  },
-  {
-    name: 'overview',
-    path: 'overview/:id',
-    component: './Overview',
-  },
-  {
-    name: 'assets',
-    path: 'assets/:id',
-    component: './Assets',
-  },
-  {
-    name: 'fspa',
-    path: 'fspa',
-    component: './fspa/layout',
-    routes: [
-      {
-        path: 'list',
-        name: 'FSPAList',
-        component: './fspa/List',
-      },
-      {
-        path: 'overview/:address',
-        name: 'FSPAOverview',
-        component: './fspa/Overview',
-      },
-      { path: '/fspa', redirect: '/fspa/list' },
-    ],
-  },
+  // {
+  //   path: '/',
+  //   name: 'home',
+  //   component: './Home',
+  // },
+  // {
+  //   name: 'account',
+  //   path: '/account',
+  //   component: './account/layout',
+  //   routes: [
+  //     {
+  //       name: 'assets',
+  //       path: 'assets',
+  //       component: './account/Assets',
+  //     },
+  //     {
+  //       name: 'plans',
+  //       path: 'plans',
+  //       component: './account/Plans',
+  //     },
+  //     {
+  //       name: 'stats',
+  //       path: 'stats',
+  //       component: './account/Stats',
+  //     },
+  //     { path: '/account', redirect: '/account/plans' },
+  //   ],
+  // },
+  // {
+  //   name: 'raising',
+  //   path: 'raising',
+  //   component: './Raising',
+  // },
+  // {
+  //   name: 'create',
+  //   path: 'create',
+  //   component: './create/layout',
+  //   routes: [
+  //     {
+  //       name: 'storage',
+  //       path: 'storage',
+  //       component: './create/Storage',
+  //     },
+  //     {
+  //       name: 'program',
+  //       path: 'program',
+  //       component: './create/Program',
+  //     },
+  //     {
+  //       name: 'benefit',
+  //       path: 'benefit',
+  //       component: './create/Benefit',
+  //     },
+  //     {
+  //       name: 'result',
+  //       path: 'result/:id',
+  //       component: './create/Result',
+  //     },
+  //     { path: '/create', redirect: '/create/storage' },
+  //   ],
+  // },
+  // {
+  //   name: 'mount',
+  //   path: 'mount',
+  //   component: './mount/layout',
+  //   routes: [
+  //     {
+  //       name: 'storage',
+  //       path: 'storage',
+  //       component: './mount/Storage',
+  //     },
+  //     {
+  //       name: 'benefit',
+  //       path: 'benefit',
+  //       component: './mount/Benefit',
+  //     },
+  //     {
+  //       name: 'result',
+  //       path: 'result/:id',
+  //       component: './mount/Result',
+  //     },
+  //     { path: '/mount', redirect: '/mount/storage' },
+  //   ],
+  // },
+  // {
+  //   name: 'overview',
+  //   path: 'overview/:id',
+  //   component: './Overview',
+  // },
+  // {
+  //   name: 'assets',
+  //   path: 'assets/:id',
+  //   component: './Assets',
+  // },
+  // {
+  //   name: 'fspa',
+  //   path: 'fspa',
+  //   component: './fspa/layout',
+  //   routes: [
+  //     {
+  //       path: 'list',
+  //       name: 'FSPAList',
+  //       component: './fspa/List',
+  //     },
+  //     {
+  //       path: 'overview/:address',
+  //       name: 'FSPAOverview',
+  //       component: './fspa/Overview',
+  //     },
+  //     { path: '/fspa', redirect: '/fspa/list' },
+  //   ],
+  // },
   // other 404
   { path: '*', redirect: '/' },
 ];
