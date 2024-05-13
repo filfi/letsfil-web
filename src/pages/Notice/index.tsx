9c3c20571a70c1275d0b7c1a527f369a732fd2a4
Steven <986535362@qq.com>
2024-05-13 14:45:45 +0800

    fix: fixed notice view

--- src/pages/Notice/index.tsx
+++ src/pages/Notice/index.tsx
@@ -20,30 +20,35 @@ export default function PageNotice() {
             <p>即日起，FilFi社区停止全部中文服务（官网和交流群），社区服务仅保留Discord和TG平台。</p>
             <p>Discord：<a href="https://discord.gg/7ARv9ZfM" target="_blank" rel="noreferrer">https://discord.gg/7ARv9ZfM</a></p>
             <p>TG：<a href="https://t.me/filfi_io" target="_blank" rel="noreferrer" >https://t.me/filfi_io</a></p>
+
+            <p className="d-flex flex-wrap gap-3">
+              <span className="my-auto">合约调用：</span>
+              <a className="btn btn-primary" href="https://public-1a8hf.filfi.io/manual-operation-cn/filfi-manual-operation.mp4" data-fancybox>
+                <span className="bi bi-play-circle"></span>
+                <span className="ms-2">观看教学视频</span>
+              </a>
+              <a
+                className="btn btn-light"
+                href="https://public-1a8hf.filfi.io/manual-operation-cn%2FFilFi%20%E7%89%B9%E6%AE%8A%E6%83%85%E5%86%B5%E6%99%BA%E8%83%BD%E5%90%88%E7%BA%A6%E6%93%8D%E4%BD%9C%E6%8C%87%E5%8D%97%202.1.pdf"
+                data-fancybox
+                data-type="pdf"
+              >
+                <span className="bi bi-file-earmark-text"></span>
+                <span className="ms-2">查看教学文档</span>
+              </a>
+              <a className="text-reset text-underline my-auto" href="#node-info" data-bs-toggle="modal">
+                查询节点信息
+              </a>
+            </p>
+
             <p className="text-end">FilFi社区</p>
             <p className="text-end">2024年5月12日</p>
           </div>
 
           <p className="mb-4 d-flex flex-column flex-md-row justify-content-md-center gap-4">
-            <a className="btn btn-primary btn-lg" href="https://public-1a8hf.filfi.io/manual-operation-cn/filfi-manual-operation.mp4" data-fancybox>
-              <span className="bi bi-play-circle"></span>
-              <span className="ms-2">观看教学视频</span>
-            </a>
-            <a
-              className="btn btn-light btn-lg"
-              href="https://public-1a8hf.filfi.io/manual-operation-cn%2FFilFi%20%E7%89%B9%E6%AE%8A%E6%83%85%E5%86%B5%E6%99%BA%E8%83%BD%E5%90%88%E7%BA%A6%E6%93%8D%E4%BD%9C%E6%8C%87%E5%8D%97%202.1.pdf"
-              data-fancybox
-              data-type="pdf"
-            >
-              <span className="bi bi-file-earmark-text"></span>
-              <span className="ms-2">查看教学文档</span>
-            </a>
           </p>
 
           <p className="text-center">
-            <a className="text-reset text-underline" href="#node-info" data-bs-toggle="modal">
-              查询节点信息
-            </a>
           </p>
         </div>
       </Fancybox>
